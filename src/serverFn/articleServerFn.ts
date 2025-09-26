adcc533047a18789a8cb824067a9590e69b9adca
carloshmccarlos <carloshmccarlos@gmail.com>
2025-09-26 10:45:47 +0800

    init

--- src/serverFn/articleServerFn.ts
+++ src/serverFn/articleServerFn.ts
@@ -26,7 +26,7 @@ export const generateArticle = createServerFn()
 
 		try {
 			const response = await ai.models.generateContent({
-				model: "gemini-2.5-flash",
+				model: "gemini-2.5-pro",
 				contents: prompt,
 
 				config: {
